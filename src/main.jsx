ab0825d08b0cd11a82c635f3c3d2f50c9a90a383
Deden Ka'bal Umam <d2n1999@gmail.com>
2025-10-20 19:20:33 +0700

    fix url #

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 import { AppProvider } from './contexts/AppContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <AppProvider>
         <App />
       </AppProvider>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 );
